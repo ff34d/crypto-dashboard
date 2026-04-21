d1dc3875c37d3f5757aece8a416d66de9fb7a4a7
ff34d <urajimirudev@gmail.com>
2026-04-21 16:42:38 +0400

    feat: connect toast to starred coin repo

--- src/entities/Coin/lib/CoinStarred.repository.ts
+++ src/entities/Coin/lib/CoinStarred.repository.ts
@@ -1,6 +1,7 @@
 "use client"
 
 import { Repository } from "@shared/lib"
+import { toast } from "sonner"
 import type { ICoin } from "../models"
 
 class CoinStarredRepository extends Repository {
@@ -9,13 +10,19 @@ class CoinStarredRepository extends Repository {
    }
 
    addStarredCoinById(id: ICoin["id"]) {
-      return this.push(id)
+      this.push(id)
+      toast.success("Coin starred")
    }
 
    removeStarredCoinById(id: ICoin["id"]) {
       const state = this.get<ICoin["id"][]>()
-      if (!state || !Array.isArray(state)) return
+
+      if (!state || !Array.isArray(state)) {
+         return toast.error(`Removed error by coin: ${id}`)
+      }
+
       this.set(state.filter((v) => v !== id))
+      toast.success("Coin removed")
    }
 
    getStarredCoinById(id: ICoin["id"]) {
